a0591f4456423bffc888e12d97e1448a54c5b65b
Kanjeng Dhimas Cahyoherlina <152190303+kanjeeng@users.noreply.github.com>
2024-08-22 23:54:17 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Analytics } from '@vercel/analytics/react';
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -20,7 +19,6 @@ export default function RootLayout({
       <head>
         <link rel="icon" href="myprofile.png" />
       </head>
-      <Analytics />
       <body className={inter.className}>{children}</body>
     </html>
   );
